1e9f4887e520056e60f37cee4ce9e4f4aeb92827
Nikita Sufyanov <niksuf1@yandex.ru>
2022-07-01 03:31:07 +0300

    Code format fix

--- LifeAlgorithm.py
+++ LifeAlgorithm.py
@@ -9,7 +9,8 @@ def numNear(ff, r, c):
             if ff[rr][cc] == '*':
                 N = N + 1
     return N
-    
+
+
 def printField(ff):
     for line in ff:
         s = ''
@@ -17,59 +18,47 @@ def printField(ff):
             s += a
         print(s)
     print()
-        
-f = []
-f2 = []
-F = open("life.txt", "r")
-for line in F:
-    line = line.replace("\n", "")
-    line = line.replace("\r", "")
-    L = list(line)
-    L2 = list(line)
-    f.append(L)
-    f2.append(L2)
-F.close()
 
-printField(f)
 
-for r in range(1, len(f) - 1):
-    for c in range(1, len(f[r]) - 1):
-        n = numNear(f, r, c)
-        f2[r][c] = str(n)
-printField(f2)
+def main():
+    f = []
+    f2 = []
+    F = open("life.txt", "r")
+    for line in F:
+        line = line.replace("\n", "")
+        line = line.replace("\r", "")
+        L = list(line)
+        L2 = list(line)
+        f.append(L)
+        f2.append(L2)
+    F.close()
 
-while True:
-    n = 0
-    for r in range(1, len(f) - 2):
+    printField(f)
+    for r in range(1, len(f) - 1):
         for c in range(1, len(f[r]) - 1):
             n = numNear(f, r, c)
-            if (n == 2 or n == 3) and (f[r][c] == '*') or ((n == 3) and (f[r][c]) == '-'):
-                f2[r][c] = '*'
-            else:
-                f2[r][c] = '-'
-    for r in range(0, len(f)):
-        for c in range(0, len(f[r])):
-            f[r][c] = f2[r][c]
-            if f[r][c] == '*':
-                n = n + 1
-    printField(f)
-    if n == 0:
-        print('YOU DIED!')
-        break
-    time.sleep(0.3)
+            f2[r][c] = str(n)
+    printField(f2)
+    while True:
+        n = 0
+        for r in range(1, len(f) - 2):
+            for c in range(1, len(f[r]) - 1):
+                n = numNear(f, r, c)
+                if (n == 2 or n == 3) and (f[r][c] == '*') or ((n == 3) and (f[r][c]) == '-'):
+                    f2[r][c] = '*'
+                else:
+                    f2[r][c] = '-'
+        for r in range(0, len(f)):
+            for c in range(0, len(f[r])):
+                f[r][c] = f2[r][c]
+                if f[r][c] == '*':
+                    n = n + 1
+        printField(f)
+        if n == 0:
+            print('YOU DIED!')
+            break
+        time.sleep(0.3)
 
 
-
-    
-    # вывести поле на экран
-    # рассчитать следующий ход (f -> f2)
-    # скопировать данные (f2 -> f)
-    # пауза на 300 мс
-
-# Ctrl+C
-
-#-----
-#--*--
-#---*-
-#-***-
-#-----
+if __name__ == '__main__':
+    main()
